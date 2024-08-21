7ef351c4540834a2d29d14fed92c300bc6ebae21
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-08-21 19:58:21 -0300

    Update Pipes

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { Item, Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 import { ImageLinks } from './../../models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 
 @Component({
   selector: 'app-lista-livros',
@@ -36,22 +37,10 @@ export class ListaLivrosComponent implements OnDestroy {
 
   // .subscribe(data => this.config = { ...data })
 
-  livrosResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = []
-
-    items.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+      return items.map(item => {
+        return new LivroVolumeInfo(item)
       })
-    })
-
-    return livros
   }
 
 
