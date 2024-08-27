b94163cb932682577658db029567f8ef2dae145c
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-08-27 18:14:18 -0300

    Update Catch Throw

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -39,8 +39,8 @@ export class ListaLivrosComponent {
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
-      debounceTime(PAUSA),
-      filter((valorDigitado) => valorDigitado.length >= 3),
+      debounceTime(PAUSA), // Inserir um pequeno delay para que dê tempo para as pessoas terminarem de digitar
+      filter((valorDigitado) => valorDigitado.length >= 3),  // Filter onde passamos uma condição que, se for satisfeita, o fluxo segue.
       tap(() => console.log('Fluxo inicial')),
       switchMap((valorDigitado) => this.service.busca(valorDigitado)),
       map(resultado => this.livrosResultado = resultado),
@@ -61,6 +61,22 @@ export class ListaLivrosComponent {
 // Tap - Operador de serviços públicos. Usado para debugging. Não modifica o observable.
 
 // Map - Operador de transformação. Transforma o observable de acordo com a função passada. Retorna um observable modificado.
+
+// switchMap - Operador de Transformação. Cancela requisições de observables anteriores, emitindo valores apenas do Observable projetado mais recentemente.
+
+// filter - Operador de filtragem. Filtra os itens emitidos pelo Observable de origem, permitindo apenas aqueles que satisfaçam uma condição especificada.
+
+// debounceTime - Operador de filtragem. Retorna um Observable que atrasa as emissões do Observable de origem pelo tempo especificado.
+
+// distinctUntilChanged - Operador de filtragem. Retorna um Observable que emite todos os valores enviados pelo observable de origem se forem distintos em comparação com o último valor emitido pelo observable de resultado.
+
+// catchError - Operador de Tratamento de Erros. Captura erros no observable manipulado retornando um novo observable ou lançando um erro.
+
+// throwError - Operador de Criação. Cria um observable que criará uma instância de erro e a enviará ao consumidor como um erro imediatamente após a assinatura.
+
+// EMPTY - Operador de Criação. Cria um Observable simples que não emite itens para o Observer e imediatamente emite uma notificação de complete.
+
+// of - Operador de Criação. Converte os argumentos em observable. Um Observable que emite os argumentos descritos e depois conclui.
 
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
